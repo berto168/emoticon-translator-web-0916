3eb3522166ebc5e54a342397dc83fe6a95389928
Robert Andrew Lin <github email address>
2016-09-23 23:51:08 -0400

    Finished

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  hash = {
+    "get_meaning" =>
+      {},
+    "get_emoticon" =>
+      {}
+  }
+  library = YAML.load_file(file)
+  library.each do |meaning, emoticons|
+    emoticons.each do |emoji|
+      if emoticons.index(emoji) == 0
+        hash["get_emoticon"][emoji] = emoticons[1]
+      else
+        hash["get_meaning"][emoji] = meaning
+      end
+    end
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  library = load_library(file)
+  if library["get_emoticon"][emoticon]
+    library["get_emoticon"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  library = load_library(file)
+  if library["get_meaning"][emoticon]
+    library["get_meaning"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
